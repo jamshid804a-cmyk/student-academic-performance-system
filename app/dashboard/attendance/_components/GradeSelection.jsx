368ad2325051f04e13b39b5433e221a18ac59738
jamshid804a <jamshid804a@gmail.com>
2026-09-18 18:32:39 +0500

    Add GradeSelection and finish attendance page

---
+++ app/dashboard/attendance/_components/GradeSelection.jsx
@@ -0,0 +1,19 @@
+"use client"
+import React from 'react'
+
+const GRADES = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"]
+
+export default function GradeSelection({ selectedGrade, defaultGrade }) {
+  return (
+    <select
+      className="border rounded-lg px-3 py-2 bg-white text-sm outline-none focus:border-blue-500"
+      value={defaultGrade || ""}
+      onChange={(e) => selectedGrade(e.target.value)}
+    >
+      <option value="">Select Grade</option>
+      {GRADES.map((g) => (
+        <option key={g} value={g}>{g}</option>
+      ))}
+    </select>
+  )
+}
